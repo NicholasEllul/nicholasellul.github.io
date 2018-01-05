efdcd94db9e80719a2fb7d81f93227477dc94c41
Nicholas Ellul <nicholas.ellul1@gmail.com>
2018-01-05 03:58:11 -0500

    Touch up about and resume sections

--- main.js
+++ main.js
@@ -2,14 +2,30 @@
 
 function updateBorder(elementId) {
 
+                alert("E");
     let element = document.getElementById(elementId);
     let style = window.getComputedStyle(element);
     let color = style.getPropertyValue('color');
-    var bordersToChange = document.getElementsByName('border');
+    let bordersToChange = document.getElementsByName('border');
+//    for(let i = 0; i < bordersToChange.length; i ++){
+
+        bordersToChange[0].style.border = '1px solid ' + color;
     
-    for(let i = 0; i < bordersToChange.length; i ++){
-        bordersToChange[i].style.border = '1px solid ' + color;
-    }
  
    
 }
+
+function changeAboutText(idOfWhatToShow) {
+    
+    let descIds = ['dev-desc', 'student-desc', 'photo-desc'];
+    
+    for(int i = 0; i < 3 i++){
+        if(descIds[i] === idOfWhatToShow){
+            document.getElementById(descIds[i]).style.display = 'block';
+        }
+        else{
+            document.getElementById(descIds[i]).style.display = 'none';
+        }
+    }
+}
+
